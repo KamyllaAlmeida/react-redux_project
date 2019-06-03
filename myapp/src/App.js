f62ee3da960a598073e91a8947013a9ac326e94d
KamyllaAlmeida <kamylla.florencio@gmail.com>
2019-06-03 16:36:07 -0700

    Conditional Output

--- myapp/src/App.js
+++ myapp/src/App.js
@@ -1,26 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component }from 'react';
+import ComponentK from './ComponentK';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component{
+  state = {
+    list :[
+      {name: 'Kamylla', age: 25, belt: "black", id: 1},
+      {name: "Mariah", age: 20, belt: "navy", id: 2},
+      {name: "Sophie", age: 27, belt: "blue", id: 3}
+    ]
+  }
+  render(){
+    return(
+      <div className="App">
+        <h1>My first React app!</h1>
+        <p>Welcome :)</p>
+        <ComponentK list = {this.state.list}/>
+      </div>
+    )
+  }
 }
 
 export default App;
